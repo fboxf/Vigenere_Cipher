f5d5f971e8fd871713b2b5b97e5a6894ad24f595
Ben <148175236+fboxf@users.noreply.github.com>
2025-02-24 15:09:10 +0100

    Add files via upload
    Major reworking of main: 
     - while loop in order to remain in program
     - memset to 0 plainText, cipherText and secretKey.

--- cipher_test.c
+++ cipher_test.c
@@ -1,59 +1,103 @@
 /*! \file cipher_test.c */
 #include <stdio.h>
+#include <string.h>
 #include <ncurses.h>
 #include "cipher.h"
 
-#define ENCRYPT
-//define DECRYPT
 //#define DEBUG
 
 int main(void){
-	char plainText[MAX_SENTENCE_LENGTH];
-	char secretKey[MAX_KEY_LENGTH];
-	char cipherText[MAX_SENTENCE_LENGTH]; 
-
 	char title[] = "Vigenere Cipher Project";
 	char encrypt_request[] = "Enter a sentence to encrypt - Maximum ";
 	char decrypt_request[] = "Enter a sentence to decrypt - Maximum ";
 	char secret_key_request[] = "Enter a secret key - Maximum ";
 
+	int ch;
+
+	char plainText[MAX_SENTENCE_LENGTH];
+	char secretKey[MAX_KEY_LENGTH];
+	char cipherText[MAX_SENTENCE_LENGTH]; 
+
 	initscr();
 
+	attron(A_REVERSE);
 	addstr(title);
+	attroff(A_REVERSE);
 
 	move(2,0);
-
-#ifdef ENCRYPT
-	printw("%s %d characters:\n", encrypt_request, MAX_SENTENCE_LENGTH);
-	refresh();
-	getnstr(plainText, MAX_SENTENCE_LENGTH);
 	
-	printw("%s %d characters:\n" , secret_key_request, MAX_KEY_LENGTH);
-	refresh();
-	getnstr(secretKey, MAX_KEY_LENGTH);
+	printw("Select an option:\n");
+	printw("1. Encrypt\n");
+	printw("2. Decrypt (q to quit) \n");
 
-	refresh();
-	encrypt(plainText, cipherText, secretKey);  // encrypt
+	while ( (ch = getch()) != 'q') {
+		
+		if (ch == '1'){
+			memset(plainText, 0, strlen(plainText));
+			memset(cipherText, 0, strlen(cipherText));
+			memset(secretKey, 0, strlen(secretKey));
 
-	printw("%s", cipherText);
-#endif
+			printw("\nEncrypt\n");
+			printw("%s %d characters:\n", encrypt_request, MAX_SENTENCE_LENGTH);
+			refresh();
 
-#ifdef DECRYPT
-	printw("%s %d characters:\n ", decrypt_request, MAX_SENTENCE_LENGTH);
-	refresh();
-	getnstr(cipherText, MAX_SENTENCE_LENGTH);
+			getnstr(plainText, MAX_SENTENCE_LENGTH);
+	
+			printw("%s %d characters:\n" , secret_key_request, MAX_KEY_LENGTH);
+			refresh();
+			getnstr(secretKey, MAX_KEY_LENGTH);
 
-	printw("%s %d characters:\n" , secret_key_request, MAX_KEY_LENGTH);
-	refresh();
-	getnstr(secretKey, MAX_KEY_LENGTH);
+			refresh();
+			encrypt(plainText, cipherText, secretKey);  // encrypt
 
-	refresh();
-	decrypt(cipherText, plainText, secretKey); // decrypt 
+			printw("%s\n", cipherText);
+			printw("(press any key to continue)");
 
-	printw("%s", plainText);
-#endif
+			getch();
 
-	getch();
+			clear();
+			refresh();
+
+			printw("Select an option:\n");
+			printw("1. Encrypt\n");
+			printw("2. Decrypt (q to quit)\n");
+		}
+		if (ch == '2'){
+			memset(plainText, 0, strlen(plainText));
+			memset(cipherText, 0, strlen(cipherText));
+			memset(secretKey, 0, strlen(secretKey));
+
+			printw("\nDecrypt\n");
+			printw("%s %d characters:\n", decrypt_request, MAX_SENTENCE_LENGTH);
+			refresh();
+			getnstr(cipherText, MAX_SENTENCE_LENGTH);
+
+			printw("%s %d characters:\n" , secret_key_request, MAX_KEY_LENGTH);
+			refresh();
+			getnstr(secretKey, MAX_KEY_LENGTH);
+
+			refresh();
+			decrypt(cipherText, plainText, secretKey); // decrypt 
+
+			printw("%s\n", plainText);
+			printw("(press any key to continue)");
+
+			getch();
+		
+			clear();
+			refresh();
+
+			printw("Select an option:\n");
+			printw("1. Encrypt\n");
+			printw("2. Decrypt (q to quit) \n");
+		}
+
+		memset(plainText, 0, strlen(plainText));
+		memset(cipherText, 0, strlen(cipherText));
+		memset(secretKey, 0, strlen(secretKey));
+
+	}
+
 	endwin(); 
 
 	return 0;
